59c7bb07fcaee44868f53832c1d7d671c0f3e184
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2021-05-16 16:21:59 +0300

    navbar media queries done

--- src/mac1/navBar/navBar.js
+++ src/mac1/navBar/navBar.js
@@ -19,31 +19,47 @@ export default function NavBar() {
           <div className={styles.item}> Services</div>
           <div className={styles.item}> Products</div>
           <div className={styles.item}> Technology</div>
-          <div className={styles.itemX}>
-            <div>Client</div>
-            <div>
-              <img className={styles.chevron} src="/chevron_down.png"></img>
+          <div className={styles.dropDown}>
+            <div className={styles.dropDownItem}>
+              <div>Client</div>
+              <div>
+                <img className={styles.chevron} src="/chevron_down.png"></img>
+              </div>
+            </div>
+            <div className={styles.popup}>
+              <div className={styles.div}>link1</div>
+              <div className={styles.div}>link2</div>
             </div>
           </div>
-          <div className={styles.itemX}>
-            <div>About</div>
-            <div>
-              <img className={styles.chevron} src="/chevron_down.png"></img>
+          <div className={styles.dropDown}>
+            <div className={styles.dropDownItem}>
+              <div>Partner</div>
+              <div>
+                <img className={styles.chevron} src="/chevron_down.png"></img>
+              </div>
+            </div>
+            <div className={styles.popup}>
+              <div className={styles.div}>link 3</div>
+              <div className={styles.div}>link 4</div>
             </div>
           </div>
+
           <div className={styles.item}> Partner</div>
         </div>
         <div className={styles.media}>
           <div>
-            <img className={styles.media1} src="/Union.png"></img>
+            <img className={styles.home} src="/Union.png"></img>
           </div>
           <div>
-            <img className={styles.media2} src="/mail.png"></img>
+            <img className={styles.mail} src="/mail.png"></img>
           </div>
           <div>
-            <img className={styles.media3} src="/shuffle.png"></img>
+            <img className={styles.shuffle} src="/shuffle.png"></img>
           </div>
         </div>
+      </div>
+      <div className={styles.phoneMenu}>
+        <img src="/text_align_right.png"></img>
       </div>
     </div>
   );
