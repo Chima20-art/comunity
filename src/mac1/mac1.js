972554efb44109be660bdf59179a94b3059e9ff4
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2021-05-16 12:18:17 +0300

    added space around

---
+++ src/mac1/mac1.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./mac1.module.css";
+import Navbar from "./navBar/navBar";
+
+export default function Mac() {
+  return (
+    <div className={styles.kl}>
+      <Navbar />
+    </div>
+  );
+}
