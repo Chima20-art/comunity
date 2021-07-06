f71259607d03625d92aed8fb29391bab71476c9e
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2021-07-06 22:58:42 +0300

    react router added

--- src/App.js
+++ src/App.js
@@ -5,16 +5,63 @@ import Navbar from "./navBar/navBar";
 import Annex from "./annex/annex";
 
 import { useState } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   const [showMenu, setShowMenu] = useState(false);
   return (
     <div className="App">
-      <Navbar showMobileMenu={showMenu} setShowMobileMenu={setShowMenu} />
-      <Mac1 />
-      <Annex />
+      <div>
+        {" "}
+        <Navbar showMobileMenu={showMenu} setShowMobileMenu={setShowMenu} />
+      </div>
+      <div>
+        <Router>
+          <div>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/about">About</Link>
+                </li>
+                <li>
+                  <Link to="/users">Users</Link>
+                </li>
+              </ul>
+            </nav>
+
+            {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+            <Switch>
+              <Route path="/about" component={Mac1} />
+
+              <Route path="/users">
+                <Users />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      </div>
+      <div>
+        <Annex />
+      </div>
     </div>
   );
 }
+function Home() {
+  return <h2>Home</h2>;
+}
 
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
 export default App;
--- src/navBar/navBar.js
+++ src/navBar/navBar.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 import { useState } from "react";
 
 import styles from "./navBar.module.css";
